ca6d5b4f4c86ee798a0192f8c352842eab55306e
pszaro <github@szaroconsulting.com>
2015-11-09 15:10:29 -0500

    Initial attempt to cleanup

---
+++ Clean-Duplicates-Open-With.sh
@@ -0,0 +1,1 @@
+/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"
