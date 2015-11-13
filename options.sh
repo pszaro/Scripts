0e026156cf8a22634fee8a45ed00e6415445c9ba
pszaro <github@szaroconsulting.com>
2015-11-12 20:59:55 -0500

    Added Create Space Option

--- options.sh
+++ options.sh
@@ -41,6 +41,9 @@ process_option() {
      'd')
      ./Clean-Duplicates-Open-With.sh
      break;;
+     's')
+     defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}' ; killall Dock 
+     break;;
     'q')
       break;;
     *)
