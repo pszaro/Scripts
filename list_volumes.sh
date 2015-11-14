1fc361915ad8ba858b291beeb93437f4ed1cc15e
pszaro <github@szaroconsulting.com>
2015-11-14 13:37:36 -0500

    Initial

---
+++ list_volumes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# What:	output a list of all mounted volumes, one line per volume.
+
+diskutil list -plist 			|\
+fgrep -A1 '<key>MountPoint</key>' 	|\
+fgrep -v '<string>/</string>' 		|\
+egrep "</string>$" 			|\
+sed 's#</string>##g ; s#.*<string>##g'
+
+exit 0
