e68a796749cfe905b0ae392527c58ccf01c4e08b
pszaro <github@szaroconsulting.com>
2015-12-27 21:55:52 -0500

    initial todo script

---
+++ todo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TARGET=~/Desktop/Dropbox/.todo
+
+if [ -d $1 ] && [ $# -gt 0 ]
+  then
+    TARGET=$1/.todo
+    shift
+fi
+
+touch $TARGET
+
+if [ $# -eq 0 ]
+  then
+    echo ; nl $TARGET ; echo
+fi
+
+if [ "$1" = "add" ]
+  then
+    shift
+    echo "$*" >> $TARGET
+    echo ; nl $TARGET ; echo
+fi
+
+if [ "$1" = "remove" ]
+  then
+    sed -i.bak -e "$2d" $TARGET
+    rm $TARGET.bak
+    echo ; nl $TARGET ; echo
+fi
+
+if [ "$1" = "clear" ]
+  then
+    echo "" > $TARGET
+    sed -i.bak -e "1d" $TARGET
+    rm $TARGET.bak
+    echo ; nl $TARGET ; echo
+fi
+
+if [ "$1" = "search" ]
+  then
+    grep -IiFnr --directories=recurse "TODO" * >> $TARGET
+    echo ; nl $TARGET ; echo
+fi
