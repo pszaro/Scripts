64fe493633a5d7ee0bc5d2e6f08598a00a8e928e
pszaro <github@szaroconsulting.com>
2017-04-22 22:02:37 -0400

    testing updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,34 @@
 
 set -e
 
+echo
+echo
+echo -=[ Starting bootstrap... ]=-
+echo
+echo
+
 # Download and install Command Line Tools
 if [[ ! -x /usr/bin/gcc ]]; then
     echo "Info   | Install   | xcode"
     xcode-select --install
 fi
+
+git clone http://github.com/pszaro/Scripts.git
+cd scripts
+./go.sh getmygit
+cd ../dotfiles
+./run.sh i
+./go.sh update
+./go.sh defaults
+./go.sh install:ctags
+./go.sh install:brewpip
+./go.sh install:software
+./go.sh enable:callhome
+
+
+
+echo
+echo
+echo -=[ Finished bootstrap ]=-
+echo
+echo
