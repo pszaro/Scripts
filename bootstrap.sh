100ba6052baab646602db3836da532de50b70885
pszaro <github@szaroconsulting.com>
2017-04-22 22:40:01 -0400

    Added sudo up front and throuout script execution

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,13 @@ echo -=[ Starting bootstrap... ]=-
 echo
 echo
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
 # Download and install Command Line Tools
 if [[ ! -x /usr/bin/gcc ]]; then
     echo "xcode not found, install..."
