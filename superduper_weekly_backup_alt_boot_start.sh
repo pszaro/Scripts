c5e864a1f92ba2a0a12d39812b1ab4cc97079f71
pszaro <github@szaroconsulting.com>
2018-02-10 10:43:05 -0500

    Added sticky to growlnotify alert

--- superduper_weekly_backup_alt_boot_start.sh
+++ superduper_weekly_backup_alt_boot_start.sh
@@ -1,3 +1,3 @@
 # Created to execute when SuperDuper runs job to alert me that it started
-/usr/local/bin/growlnotify --appIcon Xcode SuperDuper -m 'Weekly SuperDuper backup (ALT-BOOT) has started...' -i Terminal 
+/usr/local/bin/growlnotify --appIcon Xcode SuperDuper -m 'Weekly SuperDuper backup (ALT-BOOT) has started...' -i Terminal -s
 
