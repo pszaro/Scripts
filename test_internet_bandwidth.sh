6e12327513af41c259f80a43a6966fb3a3801b4c
pszaro <github@szaroconsulting.com>
2018-02-10 10:32:31 -0500

    Added some notes and added a tail to log output to display

--- test_internet_bandwidth.sh
+++ test_internet_bandwidth.sh
@@ -2,8 +2,14 @@
 #
 # Description: Using speedtest-cli, checks internet bandwidth
 #
+# Utilities Needed:
+# -speedtest-cli
+#
+
 LOG="/Users/${USER}/Desktop/Dropbox/Notes/speedtest.txt"
 echo >> ${LOG}
 echo "Executed on `hostname`" >> ${LOG}
 date >> ${LOG}
 /usr/local/bin/speedtest-cli | egrep "Download:|Upload:" >> ${LOG}
+
+tail -5 /Users/${USER}/Desktop/Dropbox/Notes/speedtest.txt
